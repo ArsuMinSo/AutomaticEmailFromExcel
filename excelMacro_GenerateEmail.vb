912d08ed3cac9575e6ed5ef101f3655df0a32f5f
Šimon Raus <77963407+ArsuMinSo@users.noreply.github.com>
2024-10-24 14:14:16 +0200

    Update excelMacro_GenerateEmail.vb

--- excelMacro_GenerateEmail.vb
+++ excelMacro_GenerateEmail.vb
@@ -1,48 +1,59 @@
 Sub GenerateEmailWithOrderAndButtons()
-    ' Definice proměnných pro Outlook
     Dim OutlookApp As Object
     Dim OutlookMail As Object
-    Dim r As Long ' Proměnná pro aktuální řádek
+    Dim r As Long
     
-    ' Definice proměnných pro data z listu
     Dim CisloObjednavky As String
     Dim PopisObjednavky As String
     Dim Ulice As String
     Dim CisloObjektu As String
     Dim CisloBytu As String
     Dim Kategorie As String
+
+    Dim UliceSCislemDomuABytu As String
     
-    ' Definice proměnných pro tělo zprávy
-    Dim TeloZpravyMisto As String
+    Dim TelozpravyMisto As String
+    Dim TextObjednavka As String
+    Dim TextPodpisPrijemce As String
+    Dim TeloObjednavka As String
+    Dim TeloTlacitka As String
+    Dim TeloPodpis As String
+    Dim TeloPata As String
     Dim TeloZpravy As String
     
-    ' Definice proměnných pro odesílatele
+    Dim TitulOdesilatele As String
     Dim JmenoOdesilatele As String
     Dim PrijmeniOdesilatele As String
     
-    ' Definice proměnných pro e-maily
+    Dim HodnostOdesilatele As String
+    Dim OdborOdesilatele As String
+    Dim OdeleniOdesilatele As String
+    Dim MestoUradu As String
+    Dim AdresaUradu As String
+    Dim TelefonOdesilatele As String
+    Dim Web As String
+    
     Dim EmailOdesilatele As String
     Dim EmailPrijemce As String
     
-    ' Definice proměnných pro zapsané informace
     Dim Zapsal As String
     Dim Prijemce As String
     
-    ' Pomocné proměnné
     Dim Radek As Long
+    
     Dim ShodaOdesilatel As Range
     Dim ShodaPrijemce As Range
     
-    ' Listy, ze kterých se čtou data
     Dim InfoListOdesilatel As Worksheet
     Dim InfoListPrijemce As Worksheet
     Dim HlavniList As Worksheet
-
-    ' Nastavení jednotlivých listů
+    
+    ' Nastavení listů
     Set InfoListOdesilatel = Sheets("InfoOdesilatel")
     Set InfoListPrijemce = Sheets("InfoPrijemce")
-    Set HlavniList = Sheets("Seznam požadavků") ' Změňte na skutečný název listu
-  
+    Set HlavniList = Sheets("Seznam požadavků") ' Změňte název na skutečný název hlavního listu
+ 
+    
     ' Získání aktuálního řádku, kde je vybraná buňka
     r = ActiveCell.Row
     
@@ -62,10 +73,18 @@ Sub GenerateEmailWithOrderAndButtons()
     ' Kontrola, zda byl odesílatel nalezen
     If Not Zapsal = "" And Not ShodaOdesilatel Is Nothing Then
         Radek = ShodaOdesilatel.Row
-        ' Načtení jména, příjmení a e-mailu odesílatele
-        JmenoOdesilatele = InfoListOdesilatel.Cells(Radek, 3).Value
-        PrijmeniOdesilatele = InfoListOdesilatel.Cells(Radek, 4).Value
-        EmailOdesilatele = InfoListOdesilatel.Cells(Radek, 5).Value
+        
+        TitulOdesilatele = InfoListOdesilatel.Cells(Radek, 3).Value
+        JmenoOdesilatele = InfoListOdesilatel.Cells(Radek, 4).Value
+        PrijmeniOdesilatele = InfoListOdesilatel.Cells(Radek, 5).Value
+        HodnostOdesilatele = InfoListOdesilatel.Cells(Radek, 6).Value
+        OdborOdesilatele = InfoListOdesilatel.Cells(Radek, 7).Value
+        OdeleniOdesilatele = InfoListOdesilatel.Cells(Radek, 8).Value
+        MestoUradu = InfoListOdesilatel.Cells(Radek, 9).Value
+        AdresaUradu = InfoListOdesilatel.Cells(Radek, 10).Value
+        TelefonOdesilatele = InfoListOdesilatel.Cells(Radek, 11).Value
+        EmailOdesilatele = InfoListOdesilatel.Cells(Radek, 12).Value
+        Web = InfoListOdesilatel.Cells(Radek, 13).Value
     Else
         ' Pokud nebyl odesílatel nalezen, zobrazí se chybová zpráva
         MsgBox ("Nebylo nalezeno žádné příjmení začínající na " & Zapsal & ".")
@@ -87,27 +106,52 @@ Sub GenerateEmailWithOrderAndButtons()
     
     ' Sestavení těla zprávy podle toho, zda je vyplněno číslo bytu
     If Not CisloBytu = "" Then
-        TeloZpravyMisto = "<p>Objednávka je na ulici: <b>" & Ulice & " v bytovém domě s číslem: " & CisloObjektu & " a číslem bytu: " & CisloBytu & "</b>.</p>"
+        TelozpravyMisto = "<p>Objednávka je na ulici: <b>" & Ulice & " v bytovém domě s číslem: " & CisloObjektu & " a číslem bytu: " & CisloBytu & "</b>.</p>"
     Else
-        TeloZpravyMisto = "<p>Objednávka je na ulici: <b>" & Ulice & " v domě s číslem: " & CisloObjektu & "</b>.</p>"
+        TelozpravyMisto = "<p>Objednávka je na ulici: <b>" & Ulice & " v domě s číslem: " & CisloObjektu & "</b>.</p>"
     End If
 
-    ' HTML kód pro tělo e-mailu s tlačítky
+    If Not CisloBytu = "" Then
+        UliceSCislemDomuABytu = "" & Ulice & " " & CisloObjektu & "_" & CisloBytu
+    Else
+        UliceSCislemDomuABytu = "" & Ulice & " " & CisloObjektu
+    End If
+
+    TextObjednavka = "Dobrý den, %0Atímto vytváříme novou objednávku číslo: " & CisloObjednavky & "%0Astručný popis objednávky: " & PopisObjednavky & "."
+    TextPodpisPrijemce = ""
+
+    TeloObjednavka = "<div><p style='margin: 0;'>Dobrý den,</p>" & _
+                "<p style='margin: 0;'>tímto vytváříme novou objednávku číslo: <b>" & CisloObjednavky & "</b>.</p>" & _
+                "<p style='margin: 0;'>stručný popis objednávky: <b>" & PopisObjednavky & "</b>.</p></div>"
+                 
+    TeloPodpis = "<div><div style='color: blue; line-height: 1;'>" & _
+                "<p style='margin-top: 30; margin-bottom: 0'><b>" & TitulOdesilatele & " " & JmenoOdesilatele & " " & PrijmeniOdesilatele & "</b></p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'>" & HodnostOdesilatele & "</p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'>" & OdborOdesilatele & "</p>" & _
+                "<p style='margin-top: 0; margin-bottom: 20'>" & OdeleniOdesilatele & "</p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'><b>" & MestoUradu & "</b></p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'>" & AdresaUradu & "</p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'>Tel: " & TelefonOdesilatele & "</p>" & _
+                "<p style='margin-top: 0; margin-bottom: 0'>Web: <a href =" & Web & ">" & Web & "</a></p></div>" & _
+                "<p style='color: green; margin-top: 20; margin-bottom: 12'>Myslete na přírodu. Skutečně potřebujete vytisknout tento e-mail?</p>" & _
+                "<p style='color: blue; margin-top: 20; margin-bottom: 12'>Obsah tohoto e-mailu včetně jeho příloh je důvěrný. Pokud nejste oprávněným adresátem tohoto emailu, <b>nejste oprávněni tuto zprávu odeslat, uložit ji, zveřejnit či naložit s ní jakýmkoliv jiným způsobem</b>. V takovém případě prosím informujte odesílatele a tento e-mail včetně jeho příloh vymažte trvale ze svého systému.</p></div>"
+
+    TeloPata = "<div><hr style='border:none; border-top:1px solid #ccc;'/>" & _
+                "<p style='font-size:11px; color:#555; font-style:italic; text-align:center;'>Tento e-mail byl automaticky generován systémem.</p>" & _
+                "<p style='font-size:11px; color:#555; text-align:center;'>Vytvořil Šimon Raus | <a href='mailto:simon.raus@email.cz' style='color:#666; text-decoration:none;'>simon.raus@email.cz</a></p></div>"
+                 
+    TeloTlacitka = "<p style='margin: 0;'>S pozdravem,<br>" & JmenoOdesilatele & " " & PrijmeniOdesilatele & "</p>" & _
+                "<p style='margin-top: 30px;'>Zároveň Vás žádáme o potvrzení této objednávky níže uvedeným tlačítkem. Jakmile budete zahajovat realizaci objednávky, tak nás opět informujte kliknutím na tlačítko „Potvrdit realizaci objednávky“</p>" & _
+                "<a href='mailto:" & EmailOdesilatele & "?subject=Objednávka " & CisloObjednavky & " " & UliceSCislemDomuABytu & "-PŘIJATO?body=" & TextObjednavka & " ' style='padding:10px 20px; background-color:#4CAF50; color:white; text-decoration:none;'>Potvrdit přijetí objednávky</a>    " & _
+                "<a href='mailto:" & EmailOdesilatele & "?subject=Objednávka " & CisloObjednavky & " " & UliceSCislemDomuABytu & "-ZAHÁJENO?body=" & TextObjednavka & "' style='padding:10px 20px; background-color:#008CBA; color:white; text-decoration:none;'>Potvrdit realizaci objednávky</a>    "
+
     TeloZpravy = "<html><body>" & _
-                 "<p>Dobrý den,</p>" & _
-                 "<p>tímto vytváříme novou objednávku číslo: <b>" & CisloObjednavky & "</b>.</p>" & _
-                 "<p>stručný popis objednávky: <b>" & PopisObjednavky & "</b>.</p>" & _
-                 TeloZpravyMisto & _
-                 "<p>S pozdravem,<br>" & JmenoOdesilatele & " " & PrijmeniOdesilatele & "</p>" & _
-                 "<br><p>Vyberte prosím jednu z následujících možností:</p>" & _
-                 "<a href='mailto:" & EmailOdesilatele & "?subject=Práce na objednávce číslo " & CisloObjednavky & " BYLA ZAČATA' style='padding:10px 20px; background-color:#4CAF50; color:white; text-decoration:none;'>Potvrdit začátek práce</a>    " & _
-                 "<a href='mailto:" & EmailOdesilatele & "?subject=Práce na objednávce číslo " & CisloObjednavky & " BYLA UKONČENA' style='padding:10px 20px; background-color:#008CBA; color:white; text-decoration:none;'>Potvrdit ukončení práce</a>    " & _
-                 "<a href='mailto:" & EmailOdesilatele & "?subject=Nějaká třetí možnost' style='padding:10px 20px; background-color:#f44336; color:white; text-decoration:none;'>Možnost 3</a>    " & _
-                 "<br><br>" & _
-                 "<hr style='border:none; border-top:1px solid #ccc;'/>" & _
-                 "<p style='font-size:9px; color:#666; font-style:italic; text-align:center;'>Tento e-mail byl automaticky generován systémem.</p>" & _
-                 "<p style='font-size:9px; color:#666; text-align:center;'>Vytvořil Šimon Raus | <a href='mailto:simon.raus@email.cz' style='color:#666; text-decoration:none;'>simon.raus@email.cz</a></p>" & _
-                 "</body></html>"
+                TeloObjednavka & _
+                TeloTlacitka & _
+                TeloPodpis & _
+                TeloPata & _
+                "</body></html>"
+
 
     ' Inicializace aplikace Outlook
     On Error Resume Next
@@ -123,7 +167,7 @@ Sub GenerateEmailWithOrderAndButtons()
     ' Nastavení e-mailu (příjemce, předmět, tělo zprávy)
     With OutlookMail
         .To = EmailPrijemce
-        .Subject = "Nová objednávka číslo: " & CisloObjednavky
+        .Subject = "Nová objednávka " & CisloObjednavky & " " & UliceSCislemDomuABytu
         .HTMLBody = TeloZpravy
         .Display ' Zobrazí e-mail, lze použít .Send pro okamžité odeslání
     End With
